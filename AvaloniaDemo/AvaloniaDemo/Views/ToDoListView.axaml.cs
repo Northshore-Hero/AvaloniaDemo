402f85d8cab94cc908b78f20622badd00f25ac11
Daniel Mattila <danmattila99@gmail.com>
2022-06-25 12:51:16 -0500

    Add Styling from Previous Project

---
+++ AvaloniaDemo/AvaloniaDemo/Views/ToDoListView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemo.Views;
+
+public partial class ToDoListView : UserControl
+{
+    public ToDoListView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
